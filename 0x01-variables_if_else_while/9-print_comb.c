458b90d4212a857492f330cf3562f5bd37d74092
TOSAN GOLD <tosangold2@gmail.com>
2023-02-18 13:51:52 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: prints all possible combinations of single-digit numbers
+ * Description: prints all possible combinations
+ * of single-digit numbers
  * Return: 0
  */
 int main(void)
