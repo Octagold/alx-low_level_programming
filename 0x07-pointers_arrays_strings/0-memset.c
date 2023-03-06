88fd47d2fef4d915ebb3fed7010bd49b5fe28d18
TOSAN GOLD <tosangold2@gmail.com>
2023-03-06 11:31:22 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to the memory area
+ * @b: constant to fill the memory with
+ * @n: bytes of the memory area to be filled
  *
- * Return: pointer to the memory area s
+ * Description: fills the first n bytes of the memory area pointed to
+ * by s with the constant b.
+ * author: Jaba
+ * date: Aug. 22, 2022. kp7c-131 room
+ * Return: pointer to the memory area s.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0. memset
+0x07-pointers_arrays_strings
